e1b9d70c3b004583fe2541a3abfd8848288b7fab
Mapscreator <nilsbroman1996@gmail.com>
2020-11-02 21:41:29 +0100

    Added some data

--- Fundamental.py
+++ Fundamental.py
@@ -12,7 +12,6 @@ def getKey(myKeyFile):
     myKey = keyFile.read()
     return myKey
 
-
 def getSymbols(market):
     print(market)
     symbols = []
@@ -32,14 +31,14 @@ def getDataFromAPI(symbol, APIKey):
     print("Symbol : ", symbol)
     test = symbol + '_overview'
     print("Concatted : ", test )
-
+    wait = 20
     for f in functions:
         params = {'function': f,
             'symbol': symbol,
             'apikey': APIKey}
         response = requests.get(base_url, params=params)
         response_dict = response.json()
-        print("AlphaVantage Limites the rate of the API call to once every 5 sec")
+        print("AlphaVantage API call (every {w} sec)".format(w = wait))
         if(f == 'OVERVIEW'):
             overview_data = {}
             overview_data["EBITDA"] = response_dict['EBITDA']
@@ -48,14 +47,18 @@ def getDataFromAPI(symbol, APIKey):
 
             relevant_data[symbol + '_overview'] = overview_data
         elif(f == 'INCOME_STATEMENT'):
+            #print(response_dict)
+            if(not response_dict['annualReports']): continue
             latest_report = response_dict['annualReports'][0]
             relevant_data[symbol + '_report'] = latest_report
         elif(f == 'BALANCE_SHEET'):
+            #print(response_dict)
+            if(not response_dict['annualReports']): continue
             #print("Balance lol")
             latest_report = response_dict['annualReports'][0]
-            print(latest_report)
+            #print(latest_report)
             relevant_data[symbol + '_report'] = latest_report
-        time.sleep(5)
+        time.sleep(wait)
     return relevant_data
 
 def writeDataToFile(data, name, end='.txt'):
@@ -79,16 +82,27 @@ def readDataFromFile(name, end='.txt'):
     file.close()
     return raw
 
-def getData(symbol, APIKey):
+def shouldUpdate(update_file):
+
+    today = time.strftime("%Y-%m-%d", time.localtime())
+    last_update = readDataFromFile(update_file)
+
+    moreThanOneYear = (int(today[:4]) - int(last_update[:4]) > 0)
+    moreThanOneMonth = (int(today[5:7]) - int(last_update[5:7]) > 0)
+    moreThanTenDays = (int(today[8:]) - int(last_update[8:]) > 10)
+
+    return moreThanOneYear or moreThanOneMonth or moreThanTenDays
+
+def getData(symbol, market, APIKey):
     relevant_data = {}
-    filename = symbol + '.txt'
-    if os.path.isfile(filename):
+    filepath = "data/" + market + "/" + symbol
+    if os.path.isfile(filepath + '.txt') or shouldUpdate("data/" + market): # or no recent update
         print("File exists for ", symbol)
-        relevant_data = readJsonFromFile("data/" + symbol)
+        relevant_data = readJsonFromFile(filepath)
     else:
         print("no file exists, creating file for ", symbol)
         relevant_data = getDataFromAPI(symbol, APIKey)
-        writeDataToFile(json.dumps(relevant_data), "data/" + symbol)
+        writeDataToFile(json.dumps(relevant_data), filepath)
     return relevant_data
 
 def getMarkets():
@@ -103,17 +117,10 @@ def getSymbols(df, market):
 
     return all_sym
 
-def getCurrentSymbols(APIKey):
+def getCurrentSymbols(APIKey):    
+    update = shouldUpdate("data/" + "Last_update")
 
-    today = time.strftime("%Y-%m-%d", time.localtime())
-    last_update = readDataFromFile("data/" + "Last_update")
-
-    moreThanOneYear = (int(today[:4]) - int(last_update[:4]) > 0)
-    moreThanOneMonth = (int(today[5:7]) - int(last_update[5:7]) > 0)
-    moreThanTenDays = (int(today[8:]) - int(last_update[8:]) > 10)
-    shouldUpdate = moreThanOneYear or moreThanOneMonth or moreThanTenDays
-
-    if(not shouldUpdate):
+    if(not update):
         print("No update")
         return
     else:
@@ -136,6 +143,19 @@ def getMarketInput(markets):
         a = a.upper()
     return a
 
+def rankFromSingle(symbols, APIKey, key, f):
+    market = 'NYSE MKT'
+    data = {}
+    for symbol in symbols:
+        all_data = getData(symbol, market, APIKey)
+        relevant = all_data[symbol + '_' + f]
+        if(relevant[key] == "None"): continue
+
+        data[symbol] = float(relevant[key])
+        print(relevant)
+
+    return sorted(data, key=data.__getitem__, reverse=True) # first best
+
 def main():
     APIKey = getKey("APIkey.txt")
 
@@ -149,9 +169,24 @@ def main():
     
     print("There is {n} symbols in the selected market ({m})".format(n = len(symbols), m = selectedMarket))
 
-    data = getData(symbols[0], APIKey)
+    print("Collecting data will take as long as {n} seconds".format(n = 15*len(symbols)))
+    data = {}
+
+    #getDataFromAPI('ATNM', APIKey)
+
+    #for symbol in symbols:
+    #    data[symbol] = getData(symbol, selectedMarket, APIKey)
+
+    
+    today = time.strftime("%Y-%m-%d", time.localtime())
+    writeDataToFile(today, "data/" + selectedMarket)
+    print(len(data))
+    
+    symbols = ['AAMC', 'AAU', 'AGE', 'AIM', 'AIRI']
+    print(rankFromSingle(symbols, APIKey, 'totalAssets', 'report')) # overview + 'EBITDA'
 
 
+    # Fix Last_update.txt file
 
 #Convert to pandas dataframe
 #df = pd.DataFrame.from_dict(response_dict, orient='index')
